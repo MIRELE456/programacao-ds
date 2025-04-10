2d5067157fd4b2ba4f6e3b17765f99efdbe54221
MIRELE456 <mirele.camargo@escola.pr.gov.br>
2025-04-10 09:21:16 -0300

    Update igualdade.py

--- igualdade.py
+++ igualdade.py
@@ -1,7 +1,7 @@
 
 # Solicita ao usuário que insira dois números
-numero1 == float(input("Digite o 1o. número: "))
-numero2 == float(input("Digite o 2o. número: "))
+numero1 = float(input("Digite o 1o. número: "))
+numero2 = float(input("Digite o 2o. número: "))
 
 # Verifica comparando se números são iguais
 if numero1 == numero2:
